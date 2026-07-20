07d9d7f00d53d77b0707fd710e784a2e5921243b
Cassandra Colaco <cacolaco@usf.edu>
2026-07-20 12:40:21 -0400

    feat: update terraform files

--- dockerfile
+++ dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.12-slim
 WORKDIR /app
-COPY requirements.txt .
-RUN pip install -r requirements.txt
-COPY . .
+COPY app/requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+COPY /app .
 EXPOSE 8080
 CMD ["python","app.py"]
---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "app" {
+  name = "myapp:latest"
+}
+
+resource "docker_container" "app" {
+  name  = "my-container"
+  image = docker_image.app.image_id
+
+  ports {
+    internal = 8080
+    external = 8080
+  }
+}
